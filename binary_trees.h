296a91739e6b1ce1b8c530147c50910839a2474c
anasyaser <anoos649@gmail.com>
2023-08-29 07:15:35 +0300

    Insert right child

--- binary_trees.h
+++ binary_trees.h
@@ -28,4 +28,6 @@ void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 /* 1-binary_tree_insert_left.c */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+/* 2-binary_tree_insert_right.c */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 #endif

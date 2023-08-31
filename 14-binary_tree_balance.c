2bede78e82c42b4f5135a5e634e31e63500cfaa6
anasyaser <anoos649@gmail.com>
2023-08-31 12:28:19 +0300

    Fix balance tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,29 @@
 #include "binary_trees.h"
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - get binary tree height
+ *
+ * @tree: pointer to tree root
+ * Return: (size_t) binary tree height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_h;
+	size_t right_h;
+
+	if (tree && (tree->left || tree->right))
+	{
+		left_h = 1 + binary_tree_height(tree->left);
+		right_h = 1 + binary_tree_height(tree->right);
+
+		if (left_h > right_h)
+			return (left_h);
+		return (right_h);
+	}
+	return (0);
+}
 
 /**
  * binary_tree_balance - measures balance factore of binary tree

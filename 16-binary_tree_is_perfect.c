8fdd6caa930324c233ca097421de2b53386720d5
anasyaser <anoos649@gmail.com>
2023-08-31 12:56:59 +0300

    Fix perfect tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,12 +11,17 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int right_l;
 	int left_l;
+	size_t right_h;
+	size_t left_h;
 
 	if (tree == NULL)
 		return (0);
 	right_l = binary_tree_is_leaf(tree->right);
 	left_l = binary_tree_is_leaf(tree->left);
-
+	right_h = binary_tree_height(tree);
+	left_h = binary_tree_height(tree);
+	if (right_h != left_h)
+		return (0);
 	if (right_l && left_l)
 		return (1);
 	else if (!right_l && !left_l)
@@ -39,3 +44,26 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	return (1);
 }
 
+/**
+ * binary_tree_height - get binary tree height
+ *
+ * @tree: pointer to tree root
+ * Return: (size_t) binary tree height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_h;
+	size_t right_h;
+
+	if (tree && (tree->left || tree->right))
+	{
+		left_h = 1 + binary_tree_height(tree->left);
+		right_h = 1 + binary_tree_height(tree->right);
+
+		if (left_h > right_h)
+			return (left_h);
+		return (right_h);
+	}
+	return (0);
+}

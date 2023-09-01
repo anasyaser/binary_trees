096142da52fff4728b2faa358e24df69de51b782
anasyaser <anoos649@gmail.com>
2023-09-01 18:30:40 +0300

    Fix bst search issue

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -12,10 +12,10 @@ bst_t *bst_search(const bst_t *tree, int value)
 {
 	if (tree == NULL)
 		return (NULL);
-	if (value == tree->n)
-		return ((bst_t *)tree);
-	else if (value > tree->n)
+	if (value > tree->n)
 		return (bst_search(tree->right, value));
 	else if (value < tree->n)
 		return (bst_search(tree->left, value));
+	return ((bst_t *)tree);
+
 }

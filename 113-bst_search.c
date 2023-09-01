f0a57547a9b247004da9923861e1c3606b8d9c91
anasyaser <anoos649@gmail.com>
2023-09-01 18:28:41 +0300

    simplify bst search

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -14,11 +14,8 @@ bst_t *bst_search(const bst_t *tree, int value)
 		return (NULL);
 	if (value == tree->n)
 		return ((bst_t *)tree);
-	else if (tree->right && value > tree->n)
+	else if (value > tree->n)
 		return (bst_search(tree->right, value));
-	else if (tree->left && value < tree->n)
+	else if (value < tree->n)
 		return (bst_search(tree->left, value));
-	else
-		return (NULL);
-
 }
